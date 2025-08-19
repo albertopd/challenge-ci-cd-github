2376f12e9c0809dd722c9a35e59a24cbb59b2f6d
Alberto <47937297+albertopd@users.noreply.github.com>
2025-08-19 10:33:19 +0200

    Adding moking of streamlit secrets

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,18 @@
 import os
+import streamlit as st
 from streamlit.testing.v1 import AppTest
 
-def test_app_title():
+
+def test_app_title(monkeypatch):
     """The app title is displayed correctly"""
+
+    # Fake secrets so Streamlit doesn't fail
+    monkeypatch.setattr(st, "secrets", {"APP_ENV": "dev"})
+
     at = AppTest.from_file("app/main.py").run()
 
-    app_env = at.secrets.get("APP_ENV", os.getenv("APP_ENV", "dev"))
+    app_env = os.getenv("APP_ENV", "dev")
     color = {"prod": "red", "qa": "orange"}.get(app_env, "green")
 
     assert at.title[0].value == f":{color}[{app_env.upper()} Environment]"
+
