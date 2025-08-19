603a55b8f20ded9f390e45972c0cff78dbc45319
Alberto <47937297+albertopd@users.noreply.github.com>
2025-08-19 10:48:15 +0200

    Rely on streamlit local secrets for the app and for tests use env variable

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,17 +2,16 @@ import os
 import streamlit as st
 from streamlit.testing.v1 import AppTest
 
+app_env = os.getenv("APP_ENV", "dev")
 
 def test_app_title(monkeypatch):
     """The app title is displayed correctly"""
 
     # Fake secrets so Streamlit doesn't fail
-    monkeypatch.setattr(st, "secrets", {"APP_ENV": "dev"})
+    monkeypatch.setattr(st, "secrets", {"APP_ENV": app_env})
 
     at = AppTest.from_file("app/main.py").run()
 
-    app_env = os.getenv("APP_ENV", "dev")
     color = {"prod": "red", "qa": "orange"}.get(app_env, "green")
 
     assert at.title[0].value == f":{color}[{app_env.upper()} Environment]"
-
